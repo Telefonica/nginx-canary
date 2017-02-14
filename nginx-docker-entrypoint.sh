99b73c4816080ba116b9b1eba7d271ee553ac5b2
jorgelg <jorgelg@tid.es>
2017-02-14 10:05:18 +0100

    fix: docker restart failed due to wrong pidfile

--- nginx-docker-entrypoint.sh
+++ nginx-docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# Remove the pidfile (it is assumed that nginx is not running when launching this script).
+# When restarting the container, nginx-canary.sh would fail if the pidfile exists and targets a wrong/unexistent process.
+rm -f /var/run/nginx.pid
+
 # Set up the configuration and versions for canary release from docker environment variables
 nginx-canary.sh
 
